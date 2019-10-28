7d342b3a6e727f1a896aebaa29a6096ef55fb4e5
Thomas Brooks <thomasbrooks@dhcp-10-250-97-253.harvard.edu>
2019-10-27 21:53:57 -0400

    start backtesting implementation

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-from forex import *
+from forex_helpers import *
 from datetime import date
 
 
 def main():
-    d = date(2018,6,1)
-    plot_rates_in_range(["USD", "USD", "USD"], ["GBP", "AUD", "CHF"], d, sma=100)
+    d = date(2007,11,1)
+    d2 = date(2009, 1, 1)
+    plot_rates_in_range(["AUD", "EUR", "GBP"], ["USD", "USD", "USD"], d, end=d2, sma=100)
 
 
 if __name__ == '__main__':
